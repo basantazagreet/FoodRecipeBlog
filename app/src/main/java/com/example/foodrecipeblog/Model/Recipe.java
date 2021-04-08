3cf87fb94352a43fc48cfbfbf1c55a99d36ee98a
Basanta Sapkota <sehwagbasanta@gmail.com>
2021-04-08 11:49:42 +0545

    models haru naayeko

---
+++ app/src/main/java/com/example/foodrecipeblog/Model/Recipe.java
@@ -0,0 +1,121 @@
+package com.example.foodrecipeblog.Model;
+
+public class Recipe {
+    private int id,likes,comments;
+    private String date,title,desc,photo,ingredients,steps,readyin,origin,category;
+    private User user;
+    private boolean selfLike;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public int getComments() {
+        return comments;
+    }
+
+    public void setComments(int comments) {
+        this.comments = comments;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String getSteps() {
+        return steps;
+    }
+
+    public void setSteps(String steps) {
+        this.steps = steps;
+    }
+
+    public String getReadyin() {
+        return readyin;
+    }
+
+    public void setReadyin(String readyin) {
+        this.readyin = readyin;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public boolean isSelfLike() {
+        return selfLike;
+    }
+
+    public void setSelfLike(boolean selfLike) {
+        this.selfLike = selfLike;
+    }
+}
